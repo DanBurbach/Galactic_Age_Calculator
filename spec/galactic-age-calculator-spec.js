4c8a2e43beb9e8ab80770c4ca91f19156447c1e1
Daniel Burbach <daniel@>
2019-01-11 09:17:33 -0800

    add basic spec tests for each planet. test for age on each planet. Currently all fail

--- spec/galactic-age-calculator-spec.js
+++ spec/galactic-age-calculator-spec.js
@@ -1,37 +1,44 @@
-import { GalacticCalc } from './../src/sudoku';
+import { GalacticCalc } from './../src/planet.js';
 
-describe('Sudoku sections', function() {
-  let GalacticCalculator;
-  beforeEach(function() {
-    sudoku = new Sudoku();
-    sudoku.setValues(Array.from(Array(81), (val, index) => index + 1));
-  });
-
-  it('should return a row', function() {
-    expect(sudoku.getRow(5)).toEqual([46,47,48,49,50,51,52,53,54]);
-  });
-  it('should return a column', function() {
-    expect(sudoku.getColumn(3)).toEqual([4,13,22,31,40,49,58,67,76]);
-  });
-  it('should return a box', function() {
-    expect(sudoku.getBox(4)).toEqual([31,32,33,40,41,42,49,50,51]);
-  });
-});
-
-describe('Sudoku values', function() {
-  let sudoku;
-  beforeEach(function() {
-    sudoku = new Sudoku();
-  sudoku.setValues([8,2,5,4,7,1,3,9,6,1,9,4,3,2,6,5,7,8,3,7,6,9,8,5,2,4,1,5,1,9,7,4,3,8,6,2,6,3,2,5,9,8,4,1,7,4,8,7,6,1,2,9,3,5,2,6,3,1,5,9,7,8,4,9,4,8,2,6,7,1,5,3,7,5,1,8,3,4,6,2,9]);
+describe('Planet age', function() {
+  let earthYear;
+  beforeEach(function(){
+    earthYear = new Planet(37);
   });
 
-  it('should return a row', function() {
-    expect(sudoku.verifySection(sudoku.getRow(5))).toEqual(true);
+  it('should capture a persons age based off of how old they are on Earth', function (){
+    expect(earthYear.planetEarth()).toEqual(37);
   });
-  it('should return a column', function() {
-    expect(sudoku.verifySection(sudoku.getColumn(3))).toEqual(true);
+
+  it('should return a persons age if they were on Mercury', function (){
+    expect(earthYear.planetMercury()).toEqual(154.17);
   });
-  it('should return a box', function() {
-    expect(sudoku.verifySection(sudoku.getBox(4))).toEqual(true);
+
+  it('should return a persons age if they were on Venus', function (){
+    expect(earthYear.planetVenus()).toEqual(60.06);
+  });
+
+  it('should return a persons age if they were on Mars', function (){
+    expect(earthYear.planetMars()).toEqual(19.68);
+  });
+
+  it('should return a persons age if they were on Jupiter', function (){
+    expect(earthYear.planetJupiter()).toEqual(3.12);
+  });
+
+  it('should return a persons age if they were on Saturn', function (){
+    expect(earthYear.planetSaturn()).toEqual(1.256);
+  });
+
+  it('should return a persons age if they were on Uranus', function (){
+    expect(earthYear.planetUranus()).toEqual(0.44);
+  });
+
+  it('should return a persons age if they were on Neptune', function (){
+    expect(earthYear.planetNeptune()).toEqual(0.22);
+  });
+
+  it('should return a persons age if they were on Pluto', function (){
+    expect(earthYear.planetPluto()).toEqual(0.149);
   });
 });
